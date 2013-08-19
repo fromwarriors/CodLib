825b3029fa468b17c0984cd0c88fd209d78a6200
Michael Oselskyi <oselskyi@gmail.com>
2013-08-18 20:44:00 -0700

    Update codlib.py
    -Added the logo into a separate file for space management. Started work on the csv stuff.

--- codlib.py
+++ codlib.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# CodLib v1.26a
+# CodLib v1.28a
 # Programmer: Michael Oselskyi & Natan L
 #DISCLAIMER: NOT COMPLETE! GUI soon though.
 # UPDATE 5/14/13: Added function to search through the sql file.
 # Update 8/9/13: Added new-style classes, fixed the Ascii Art.
 # Update 8/11/13: Fixed Menus(MAJOR BUG FIX). Fine Tuning.
+# Update 8/18/13: Added the logo into a seperate file for space managment. Started work on the csv stuff.
 
 import os
 import sys
-import string # Unused?
 import functools
+import Logo
+import csv
 
 
 def _clear_screen():
@@ -26,28 +28,28 @@ def exit():
 
 def search_file(filename):
     try:
+        print("Enter search term:")
         query = input('CodLib> ').lower()
     except EOFError:
         print('\nExiting...\n')
         sys.exit()
-    with open(filename) as txt:
-        for line in txt:
+    with open(filename) as file:
+        for line in file:
             if query in line:
                 print(line)
 
     search()
 
 def search(): # [RESOLVED]
-    print("Please choose your prog lang\n")
+    print("Please choose your prog lang\n(1)Python\n(2)SQL[i]\n")
     options = {'1': functools.partial(search_file, 'python.txt'), 
                '2': functools.partial(search_file, 'sql.txt'),'e': sys.exit}
     while 1:
-        prompt = "(1)Python or (2)SQL? "
-        choice = str(input(prompt)).lower()
+        choice = str(input("CodLib> ")).lower()
         try:
             options[choice]()
         except KeyError:
-            print("'%s': invalid option. Try Again." % choice)
+            print("'{0}': invalid option. Try Again." .format(choice))
     options.get(options, _invalid_prompt)()
 
 class applySnip(object):
@@ -67,7 +69,7 @@ class applySnip(object):
             try:
                  options[choice]()
             except KeyError:
-                print("'%s': invalid option. Try Again." % choice)
+                print("'{0}': invalid option. Try Again." .format(choice))
         options.get(options, _invalid_prompt)()
     def pyAppl(self):
         print("Is your snippet multilined? y/n")
@@ -78,15 +80,15 @@ class applySnip(object):
                                 Snippet:What it does. Type ^C when done")
             line = 0
             while line < 1000:
-                line += 1
-                snippet = input("codlib> ")
-                with open("python.txt", "a") as f:
-                    f.write("\n" + snippet)
+                line += 1  
+                snippet = input("CodLib> ")
+                with open('python.txt','a') as f:
+                    f.write("\n"+ snippet)
             print("Done!")
             snip.appl()
         elif option == 'n':
-            print("Please write your snippet in this format: \
-                                Snippet:What it does.")
+            print("Please write your snippet in this format:\
+ Snippet:What it does.")
             snippet = input("codlib> ")
             print("Done!")
             snip.appl()
@@ -111,9 +113,8 @@ class applySnip(object):
             print("Please write your snippet in this format: \
  Snippet:What it does.")
             snippet = input("codlib> ")
-            if snippet == '/end/':
-                print("Done!")
-                mainMenu()
+            print("Done!")
+            snip.appl()
             with open("sql.txt", "a") as f:
                 f.write("\n" + snippet)
 
@@ -123,27 +124,13 @@ snip = applySnip()
 
 def mainMenu():
     _clear_screen() #Will clear the terminal to avoid clutter.
-    print("""
- ___  _____  ____  __    ____  ____ 
-/ __)(  _  )(  _ \(  )  (_  _)(  _ \                   
-(  (__)(_)(  )(_) ))(__ _ )(_  ) _ <           
-\___)(_____)(____/(____)(____)(____/
-""")
-    
-    options = ['Search','Apply','Exit']
+    Logo.logo()
     callbacks = [search,snip.appl,exit]
-        
-    for i, option in enumerate(options):
-        print('%s. %s' % (i, option)) #Display all options
-    try:
-        choice = int(input('Enter Your Choice: '))
-    except KeyError:
-        print("'%s':invalid option. Try again." % choice)
+    choice = int(input('Enter Your Choice: ')) 
     try:
         callbacks[choice]() # call cooresponding options
     except IndexError:
-        print("'%s':invalid option. Try again later." % choice)
-        
+        print('"%s":invalid option. Try Again.'% (choice))
     
 if __name__ == '__main__':
     mainMenu()
